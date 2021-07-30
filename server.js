5945780c4a7671c83c158fcb84c174132c939cdd
LostTripper03 <62254523+LostTripper03@users.noreply.github.com>
2021-07-30 08:47:03 +0200

    Konfig für Heroku angepasst https://betterprogramming.pub/how-to-deploy-your-angular-9-app-to-heroku-in-minutes-51d171c2f0d

---
+++ server.js
@@ -0,0 +1,19 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'http') {
+        return res.redirect('http://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const app = express();
+//app.use(requireHTTPS);
+
+app.use(express.static('./dist/htlw'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/htlw/'}
+);
+});
+
+app.listen(process.env.PORT || 8080);
