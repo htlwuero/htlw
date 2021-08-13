e53dca14e3c5eb74a12f3e2c0806e80b8cdb3596
Sam Smith <sam@example.com>
2021-08-13 16:04:00 +0200

    datenbank anzeigen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "../models/employee";
+import {DataService} from "../services/data.service";
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'htlw';
+  employees : Employee[] = [];
+
+  constructor(private dataService:DataService) {
+
+  }
+
+  ngOnInit(): void {
+    this.dataService.getAllEmployees().subscribe(
+      data => {this.employees = data;
+      console.log(data);
+      }
+    )
+  }
 }
